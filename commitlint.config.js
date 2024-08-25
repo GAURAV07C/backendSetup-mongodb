769ec0682ab6a02f492889e19266c65bb46402c7
Gaurav Kumar <gaurav989267@gmail.com>
2024-08-25 12:53:11 +0530

    feat: Commitlint  Setup

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends:["@commitlint/cli","@commitlint/config-conventional"],
+    rules:{
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "perf",
+                "test",
+                "build",
+                "ci",
+                "chore",
+                "revert",
+            ]
+        ],
+        "subject-case":[2,"always","sentence-case"]
+    }
+}
