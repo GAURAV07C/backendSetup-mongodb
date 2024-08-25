82ae6cef4fde1f51a3ec335496342b42f822766b
Gaurav Kumar <gaurav989267@gmail.com>
2024-08-25 15:44:24 +0530

    feat: Setup eslint and check

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    extends:["@commitlint/cli","@commitlint/config-conventional"],
-    rules:{
+    extends: ["@commitlint/cli", "@commitlint/config-conventional"],
+    rules: {
         "type-enum": [
             2,
             "always",
@@ -9,14 +9,15 @@ module.exports = {
                 "fix",
                 "docs",
                 "style",
+                "refactor",
                 "perf",
                 "test",
                 "build",
                 "ci",
                 "chore",
-                "revert",
+                "revert"
             ]
         ],
-        "subject-case":[2,"always","sentence-case"]
+        "subject-case": [2, "always", "sentence-case"]
     }
 }
